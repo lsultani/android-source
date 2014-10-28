81af947e4156527dac269f3f08d7b3833f8854e7
lsultani <lesliesultani@msn.com>
2014-10-27 17:21:36 -0700

    singleton assignment

--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
@@ -1,10 +1,29 @@
 package com.bloc.singletons;
 
-public class Main extends Object {
+import com.bloc.singletons.listeners.*;
+import com.bloc.singletons.talkers.*;
 
-	public static void main(String [] args) {
-		// Instantiate some talkers and some listeners
-		// Register listeners
-		// Send messages!
-	}
-}
+    public class Main extends Object {
+
+        public static void main(String [] args) {
+            // Instantiate some talkers and some listeners
+            //Talkers: Parent, PetOwner, Singer
+            //Listeners: AudienceMmber, Child, Pet
+
+            Talker parent = new Parent();
+            Talker singer = new Singer();
+            Listener audienceMember = new AudienceMember();
+            Listener child = new Child();
+
+            Speakerphone speakerphone = Speakerphone.get();
+
+            // Register listeners
+            speakerphone.addListener(audienceMember);
+            speakerphone.addListener(child);
+
+            // Send messages!
+            //shoutMessage(Talker talker,Class<?> cls)
+            speakerphone.shoutMessage(singer, AudienceMember.class);//pass talker, pass an listener obj class
+            speakerphone.shoutMessage(parent, Child.class);//pass talker, pass an listener obj class
+        }
+    }
--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
@@ -1,48 +1,98 @@
 package com.bloc.singletons;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 /*
  * This is a singleton class which maintains communication
  * between classes and Listeners
  */
 public class Speakerphone extends Object {
-	/*
-	 * get
-	 * @return the singleton instance of Speakerphone
-	 */
+    //static instance
+    private static Speakerphone speakerphone;
 
-	/*
-	 * addListener
-	 * Add a listener to Speakerphone's list
-	 * @param listener an instance of the Listener interface
-	 */
 
-	/*
-	 * removeListener
-	 * Remove a Listener from the Speakerphone's list
-	 * @param listener the Listener to remove
-	 */
+    /*
+     * get
+     * @return the singleton instance of Speakerphone
+     */
+    //public getter
+    public static Speakerphone get() {
+        if (speakerphone == null) {
+            speakerphone = new Speakerphone();
+        }
+        return speakerphone;
+    }
 
-	/*
-	 * shoutMessage
-	 * Sends the message to all of the Listeners tracked by Speakerphone
-	 * @param talker a Talker whose message will be sent
-	 */
+    //set of listeners
+    private HashSet<Listener> listenersSet;
 
-	/*
-	 * shoutMessage
-	 * Sends the message to all of the Listeners which are instances of
-	 * the class parameter
-	 * @param talker a Talker whose message will be sent
-	 * @param cls a Class object representing the type which the Listener
-	 *			  should extend from in order to receive the message
-	 *
-	 * HINT: see Class.isAssignableFrom()
-	 *		 http://docs.oracle.com/javase/7/docs/api/java/lang/Class.html#isAssignableFrom(java.lang.Class)
-	 */
+    private Speakerphone() {
+        listenersSet = new HashSet<Listener>();
+    }
 
-	/*
-	 * removeAll
-	 * Removes all Listeners from Speakerphone
-	 */
+    /*
+     * addListener
+     * Add a listener to Speakerphone's list
+     * @param listener an instance of the Listener interface
+     */
+    public void addListener(Listener listener) {
+        listenersSet.add(listener);
+    }
+
+
+    /*
+     * removeListener
+     * Remove a Listener from the Speakerphone's list
+     * @param listener the Listener to remove
+     */
+    public void removeListener(Listener listener) {
+        listenersSet.remove(listener);
+    }
+
+    /*
+     * shoutMessage
+     * Sends the message to all of the Listeners tracked by Speakerphone
+     * @param talker a Talker whose message will be sent
+     */
+    public void shoutMessage(Talker talker) {
+        Iterator<Listener> listenIterator = listenersSet.iterator();
+        //better to use iterato, if removing items
+        while (listenIterator.hasNext()) {
+            listenIterator.next().onMessageReceived(talker.getMessage());
+            //pass talker message
+        }
+
+    }
+
+    /*
+     * shoutMessage
+     * Sends the message to all of the Listeners which are instances of
+     * the class parameter
+     * @param talker a Talker whose message will be sent
+     * @param cls a Class object representing the type which the Listener
+     *			  should extend from in order to receive the message
+     *
+     * HINT: see Class.isAssignableFrom()
+     *		 http://docs.oracle.com/javase/7/docs/api/java/lang/Class.html#isAssignableFrom(java.lang.Class)
+     */
+    public void shoutMessage(Talker talker, Class<?> cls) {
+        Iterator<Listener> listenIterator = listenersSet.iterator();
+        while (listenIterator.hasNext()) {
+            Listener listen = listenIterator.next();
+            if (cls.isAssignableFrom(listen.getClass())) {
+                listen.onMessageReceived(talker.getMessage());
+                //pass talker message
+            }
+        }
+    }
+
+    /*
+     * removeAll
+     * Removes all Listeners from Speakerphone
+     */
+    public void removeAll() {
+        listenersSet.clear();
+    }
 
 }
