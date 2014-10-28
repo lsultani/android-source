fbc13577fa3819e3e6368cf107e45e9c9b8435dd
lsultani <lesliesultani@msn.com>
2014-10-27 19:34:34 -0700

    threads check in

--- milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
+++ milestone_01_java/02_nuance/threads/src/com/bloc/threads/Main.java
@@ -11,38 +11,19 @@ public class Main extends Object {
 
 		// Extract content beneath to ImageGetter
 
-		try {
-			File existingImage = new File("google_logo.png");
-			if (existingImage.exists()) {
-				existingImage.delete();
-			}
-			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
-			BufferedImage bufferedImage = ImageIO.read(url);
-			File outputfile = new File("google_logo.png");
-			ImageIO.write(bufferedImage, "png", outputfile);
-			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
-				Runtime.getRuntime().exec("open google_logo.png");
-			} else {
-				Runtime.getRuntime().exec("google_logo.png");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-
-		File logo = new File("google_logo.png");
-		boolean exists = false;
-		try {
-			exists = logo.exists();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+        ImageGetter thread = new ImageGetter();
+        thread.start();
 
 		// ^^^ Extract the above to ImageGetter
+
+        File logo = new File("google_logo.png");
+        boolean exists = false;
+        try {
+            exists = logo.exists();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
 		// This shouldn't exist yet, therefore we should be able to print 
 		if (exists == false) {
