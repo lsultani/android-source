e27b3075a9e4033af165a4b2c288f3c707b4c7bd
lsultani <lesliesultani@msn.com>
2014-10-28 20:31:17 -0700

    created new NoteFragment

---
+++ BlocNotes/app/src/main/java/com/bloc/blocnotes/NoteFragment.java
@@ -0,0 +1,14 @@
+package com.bloc.blocnotes;
+
+import android.app.Fragment;
+
+/**
+ * Created by lesliesultani on 10/28/14.
+ */
+public class NoteFragment extends Fragment {
+
+    NoteFragment() {
+        // Emptiness
+    }
+
+}
