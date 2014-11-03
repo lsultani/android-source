14be69fc586ee28ad09c6aae7280c819688ec65a
lsultani <lesliesultani@msn.com>
2014-11-03 10:12:29 -0800

    views

--- BlocNotes/app/src/main/java/com/bloc/blocnotes/NoteFragment.java
+++ BlocNotes/app/src/main/java/com/bloc/blocnotes/NoteFragment.java
@@ -1,6 +1,10 @@
 package com.bloc.blocnotes;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by lesliesultani on 10/28/14.
@@ -9,6 +13,13 @@ public class NoteFragment extends Fragment {
 
     public NoteFragment() {
         // Emptiness
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.notefragment, container, false);
+        return rootView;
     }
 
 }
