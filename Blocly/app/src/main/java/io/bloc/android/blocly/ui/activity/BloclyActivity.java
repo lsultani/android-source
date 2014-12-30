160ac64d70d588805bc60e7fac329972e68b44c2
lsultani <lesliesultani@msn.com>
2014-12-29 17:20:29 -0800

    initial commit

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,22 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
+
+/**
+ * Created by lesliesultani on 12/29/14.
+ */
+public class BloclyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(int) finds the layout file associated with the given resource
+        // identifier and inflates it into a programmatic View which can be presented to the user.
+        // res/ folder is dynamically assigned a unique, static number. This process is done every
+        // time we build an application or whenever a new asset is created. R.layout.activity_blocly
+        // refers to the unique number generated specifically for the activity_blocly.xml layout file.
+        setContentView(R.layout.activity_blocly);
+    }
+}
