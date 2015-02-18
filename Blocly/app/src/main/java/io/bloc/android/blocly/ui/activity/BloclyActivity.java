8d11af6e310f578bb9e83c715b431bf7c64cc250
lsultani <lesliesultani@msn.com>
2015-02-17 16:20:32 -0800

    getting error in styles.xml with AppCompat colorPrimary

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,10 +1,15 @@
 package io.bloc.android.blocly.ui.activity;
 
-import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.ui.adapter.ItemAdapter;
@@ -12,36 +17,59 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 /**
  * Created by lesliesultani on 12/29/14.
  */
-public class BloclyActivity extends Activity {
+public class BloclyActivity extends ActionBarActivity {
 
     private ItemAdapter itemAdapter;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(int) finds the layout file associated with the given resource
-        // identifier and inflates it into a programmatic View which can be presented to the user.
-        // res/ folder is dynamically assigned a unique, static number. This process is done every
-        // time we build an application or whenever a new asset is created. R.layout.activity_blocly
-        // refers to the unique number generated specifically for the activity_blocly.xml layout file.
         setContentView(R.layout.activity_blocly);
 
-       // Toast.makeText(this,
-       //         BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
-       //         Toast.LENGTH_LONG).show();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_activity_blocly);
+        setSupportActionBar(toolbar);
 
         itemAdapter = new ItemAdapter();
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
 
-        //A LayoutManager organizes ViewHolders and their respective Views within a RecyclerView.
-        // We've chosen LinearLayoutManager due to the fact that it's already implemented and orders
-        // items in a list much like the one we want.
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        // An ItemAnimator is responsible for animating each View within the RecyclerView as it is added or removed
+
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(itemAdapter);
 
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            drawerLayout = (DrawerLayout) findViewById(R.id.dl_activity_blocly);
+
+            drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, 0, 0);
+            drawerLayout.setDrawerListener(drawerToggle);
     }
+
+
+        @Override
+        protected void onPostCreate(Bundle savedInstanceState) {
+            super.onPostCreate(savedInstanceState);
+            drawerToggle.syncState();
+        }
+
+
+        @Override
+        public void onConfigurationChanged(Configuration newConfig) {
+            super.onConfigurationChanged(newConfig);
+            drawerToggle.onConfigurationChanged(newConfig);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            if (drawerToggle.onOptionsItemSelected(item)) {
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+
+
 }
